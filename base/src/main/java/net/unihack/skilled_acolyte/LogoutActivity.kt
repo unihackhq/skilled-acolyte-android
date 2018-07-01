184340015db6a7aed6efc90641697a1643a12b00
Robert Koch <robert@kochie.io>
2018-07-02 09:20:51 +1000

    Added API Service to a seperate feature module

---
+++ base/src/main/java/net/unihack/skilled_acolyte/LogoutActivity.kt
@@ -0,0 +1,12 @@
+package net.unihack.skilled_acolyte
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LogoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_logout)
+    }
+}
