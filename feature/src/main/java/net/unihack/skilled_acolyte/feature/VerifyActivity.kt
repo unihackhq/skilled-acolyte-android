d05bd04b13e2ec0322caeca2037eea2fb96b3b86
Robert Koch <robert@kochie.io>
2018-07-01 19:31:49 +1000

    Added verify activity to verify new users

---
+++ feature/src/main/java/net/unihack/skilled_acolyte/feature/VerifyActivity.kt
@@ -0,0 +1,12 @@
+package net.unihack.skilled_acolyte.feature
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class VerifyActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_verify)
+    }
+}
